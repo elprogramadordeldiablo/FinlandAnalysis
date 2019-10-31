26b99d6ae8b16e632cdf315026d3d2beda48d13b
elprogramadordeldiablo <57001455+elprogramadordeldiablo@users.noreply.github.com>
2019-10-31 15:22:07 -0100

    Changing the source file to test

--- Results_GLM.R
+++ Results_GLM.R
@@ -1,34 +1,33 @@
-ResultsGLM <- read.csv("RESULTS.csv", sep=",", header = TRUE)
-str(ResultsGLM)
 
-glmA1<-glm(BetaAllTotalVector ~  treatment + Dist_edge, data = ResultsGLM, family = binomial)
+
+glmA1<-glm(BetaAllTotalVector ~  treatment + Dist_edge, data = Results2, family = binomial)
 summary(glmA1)
 
-glmA2<-glm(BetaAllTotalVector ~  treatment, data = ResultsGLM, family = binomial)
+glmA2<-glm(BetaAllTotalVector ~  treatment, data = Results2, family = binomial)
 summary(glmA2)
 
-glmA3<-glm(BetaAllTotalVector ~ ForestID, data = ResultsGLM, family = binomial )
+glmA3<-glm(BetaAllTotalVector ~ ForestID, data = Results2, family = binomial )
 summary(glmA3)
 
-glmA4<-glm(BetaAllTotalVector ~ Dist_edge, data = ResultsGLM, family = binomial)
+glmA4<-glm(BetaAllTotalVector ~ Dist_edge, data = Results2, family = binomial)
 summary(glmA4)
 
-glmA5<-glm(BetaAllTotalVector ~ Dist_trail_beginning, data = ResultsGLM, family = binomial() )
+glmA5<-glm(BetaAllTotalVector ~ Dist_trail_beginning, data = Results2, family = binomial() )
 summary(glmA5)
 
-glmA6<-glm(BetaAllTotalVector ~ Dist_trail, data = ResultsGLM, family = binomial() )
+glmA6<-glm(BetaAllTotalVector ~ Dist_trail, data = Results2, family = binomial() )
 summary(glmA6)
 
-glmA7<-glm(BetaAllTotalVector ~ Dist_trail + Dist_trail_beginning + Dist_edge + ForestID + treatment, data = ResultsGLM, family = binomial() )
+glmA7<-glm(BetaAllTotalVector ~ Dist_trail + Dist_trail_beginning + Dist_edge + ForestID + treatment, data = Results2, family = binomial() )
 summary(glmA7)
 
-glmA8<-glm(BetaAllTotalVector ~ Dist_trail + Dist_trail_beginning + ForestID + treatment, data = ResultsGLM, family = binomial() )
+glmA8<-glm(BetaAllTotalVector ~ Dist_trail + Dist_trail_beginning + ForestID + treatment, data = Results2, family = binomial() )
 summary(glmA8)
 
-glmA9<-glm(BetaAllTotalVector ~ Dist_trail + Dist_trail_beginning + Dist_edge + ForestID + treatment, data = ResultsGLM, family = binomial() )
+glmA9<-glm(BetaAllTotalVector ~ Dist_trail + Dist_trail_beginning + Dist_edge + ForestID + treatment, data = Results2, family = binomial() )
 summary(glmA9)
 
-glmA10<-glm(BetaAllTotalVector ~ Dist_trail + Dist_trail_beginning + Dist_edge + ForestID + treatment, data = ResultsGLM, family = binomial)
+glmA10<-glm(BetaAllTotalVector ~ Dist_trail + Dist_trail_beginning + Dist_edge + ForestID + treatment, data = Results2, family = binomial)
 summary(glmA10)
 
 
@@ -36,21 +35,21 @@ summary(glmA10)
 
 #Beta Functional
 
-glmB1<-glm(BetaAllTotalVector ~  treatment + ForestID, data = ResultsGLM )
+glmB1<-glm(BetaAllTotalVector ~  treatment + ForestID, data = Results2 )
 summary(glmB1)
 
-glmB2<-glm(BetaAllTotalVector ~  treatment, data = ResultsGLM )
+glmB2<-glm(BetaAllTotalVector ~  treatment, data = Results2 )
 summary(glmB2)
 
-glmB3<-glm(BetaAllTotalVector ~ ForestID, data = ResultsGLM )
+glmB3<-glm(BetaAllTotalVector ~ ForestID, data = Results2 )
 summary(glmB3)
 
-glmB4<-glm(BetaAllTotalVector ~ Dist_edge, data = ResultsGLM )
+glmB4<-glm(BetaAllTotalVector ~ Dist_edge, data = Results2 )
 summary(glmB4)
 
-glmB5<-glm(BetaAllTotalVector ~ Dist_trail_beginning, data = ResultsGLM )
+glmB5<-glm(BetaAllTotalVector ~ Dist_trail_beginning, data = Results2 )
 summary(glmB5)
 
-glmB6<-glm(BetaAllTotalVector ~ Dist_trail, data = ResultsGLM )
+glmB6<-glm(BetaAllTotalVector ~ Dist_trail, data = Results2 )
 summary(glmB6)
 
